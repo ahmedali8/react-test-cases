59083dcd538dfeb56a766305a5d7b90e5bcd82ea
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-06 18:50:34 +0500

    updated App file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div className="App">Hello World Hello Ahmed</div>;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello World/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders hello ahmed', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello Ahmed/i);
   expect(linkElement).toBeInTheDocument();
 });
